a407bc76a6327907815888abf792323eca7e4e53
LegionarioDeAcero <LegionarioDeAcero@c6979bec-28b4-541b-10ca-921024090cd1>
2011-09-17 21:18:09 +0000

    QR mediante rotaciones de Givens. Me tira el mismo error de memoria cuando quiero ejecutar prob3(arrobagivens). Probé qu8e ande la descomposición QR comparando los resultados míos con los que Householder para mismas matrices A y daban bien

---
+++ trunk/tp1/src/givens.m
@@ -0,0 +1,27 @@
+% Using QR Givens' method.
+%	A is mxn with m >= n
+function [Q R] = givens(A)
+	m = length(A);
+	n = length(A(:,1));
+
+	Q = eye(m,m);
+	R = A;
+	
+	% Bottom-up until diagonal, then left-right.
+	for j = 1:n;
+		for i = m:-1:j+1
+			if (R(i,j) != 0)
+				a = R(i-1,j);
+				b = R(i,j);
+				d = sqrt(a^2 + b^2);
+				c = a / d;
+				s = -b / d;
+
+				G = givensrot(i, j, c, s, m);
+				R = G * R;
+				Q = Q * G';
+			endif
+		endfor
+	endfor
+
+endfunction
--- trunk/tp1/src/prob2.m
+++ trunk/tp1/src/prob2.m
@@ -1,6 +1,8 @@
-data = load("../data/windms.data");
-j = 0;
-for i=4:length(data(1,:))
-	j = j+1;
-	avgdata(j) = sum(data(:,i))/length(data(:,1));
-end
+function avgdata = prob2()
+	data = load("../data/windms.data");
+	j = 0;
+	for i=4:length(data(1,:))
+		j = j+1;
+		avgdata(j) = sum(data(:,i))/length(data(:,1));
+	end
+endfunction
