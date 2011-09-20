85cc881695ab9ca30f4d002cbfb52fba674e36c1
alejandromagnorsky <alejandromagnorsky@c6979bec-28b4-541b-10ca-921024090cd1>
2011-09-20 16:39:33 +0000

    Un poco mas del informe

--- tp1/src/householder.m
+++ tp1/src/householder.m
@@ -1,20 +1,20 @@
 function [Q,R] = householder(A)
+	[m, n] = size(A);
+	Q = eye(m);
+	R = A;
 
-[m, n] = size(A);
-Q = eye(m);
-R = A;
+	for k=1:n
+		if(k >= m)
+			Q = Q';
+			return;
+		endif
+		x = R(k:m,k);
+		u = x;
+		u(1) = u(1)+sign(x(1))*norm(x);
+		H = eye(m-(k-1)) - (2/norm(u)^2).*(u*u');
 
-for k=1:n
-	if(k >= m)
-		Q = Q';
-		return;
-	end
-	x = R(k:m,k);
-	u = x;
-	u(1) = u(1)+sign(x(1))*norm(x);
-	H = eye(m-(k-1)) - (2/norm(u)^2).*(u*u');
-
-	R(k:m,:) = H*R(k:m,:);
-	Q(k:m,:) = H*Q(k:m,:);
-end
-Q = Q';
+		R(k:m,:) = H*R(k:m,:);
+		Q(k:m,:) = H*Q(k:m,:);
+	endfor
+	Q = Q';
+endfunction
