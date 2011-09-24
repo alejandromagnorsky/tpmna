3e2a4f8e17132cfec223038a5e774790122f26d6
alejandromagnorsky <alejandromagnorsky@c6979bec-28b4-541b-10ca-921024090cd1>
2011-09-24 16:56:54 +0000

    Mas del informe

--- trunk/tp1/src/householder.m
+++ trunk/tp1/src/householder.m
@@ -1,23 +1,27 @@
 % A es una matriz de mxn con m >= n
 % b es un vector de mx1
+% Devuelve el vector x de nx1 / R1*x = Q1'*b
 function [x,e] = householder(A,b)
 	[m, n] = size(A);
 	R = A;
 	
 	for k=1:n
 		x = R(k:m,k);
-		u = x;
+		% ||x||^2 = <x,x> = x'*x
 		nx2 = x'*x;
 		sigma = sign(x(1))*sqrt(nx2);
+		u = x;
 		u(1) = u(1)+sigma;
+
 		H = eye(m-(k-1)) - (1/(nx2+x(1)*sigma)).*(u*u');
 
 		R(k,k) = -sigma;
 		R(k+1:m,k) = 0;
 		if k < n
-			R(k:m,k+1:n) = H(:,:)*R(k:m,k+1:n);
+			R(k:m,k+1:n) = H*R(k:m,k+1:n);
 		endif
-		% Calcula Q1T*b en lugar de calcular Q
+
+		% Calcula Q1'*b en lugar de calcular Q
 		b(k:m) = H*b(k:m);
 	endfor
 
