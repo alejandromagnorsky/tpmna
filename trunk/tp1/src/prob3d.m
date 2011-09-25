3ed4594b4657da835927e737aa101b7b0156c3d0
merchante.mariano <merchante.mariano@c6979bec-28b4-541b-10ca-921024090cd1>
2011-09-25 23:14:23 +0000

    Histograma

--- trunk/tp1/src/prob3d.m
+++ trunk/tp1/src/prob3d.m
@@ -1,4 +1,9 @@
 function prob3d()
+
+	names = [ "Roche's Pt." ; "Valentia" ; "Rosslare" ; 
+		 "Kilkenny" ; "Shannon"; "Birr"; "Dublin" ;
+		"Claremorris"; "Mullingar"; "Clones"; "Belmullet";"Malin head" ];
+
 	[x,e] = prob3a();
 	data = load("../data/windms.data");
 	n = length(data(:,1));
@@ -12,7 +17,9 @@ function prob3d()
                 for i = 1:n
 			e(i) = data(i,j) - v(t(i), x(1,j), x(2,j),x(3,j));
                 endfor
-		figure(j);
+		subplot(4,3,j);
 		hist(e);
+		title(names(j,:));
         endfor
+	print -dpng "-S1100,720" prob_3d.png
 endfunction
