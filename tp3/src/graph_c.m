f17adbcdbd88a0edaa40affdd2a37e1f11e7b146
merchante.mariano <merchante.mariano@c6979bec-28b4-541b-10ca-921024090cd1>
2011-11-28 14:10:07 +0000

    Ejercicio c) terminado.

---
+++ tp3/src/graph_c.m
@@ -0,0 +1,29 @@
+function graph_c(deltaR, deltaT)
+
+	v = centeredDifferences(deltaR, deltaT);
+	
+	r = .6:.9;
+	
+	rTotal = (1/2)/deltaR;
+	tTotal = 10/deltaT;
+	
+	rStep = .1/deltaR;
+	
+	hold
+	for i=1:rStep:rTotal-rStep
+		subplot(2,2,i);
+		plot(0:deltaT:10,v(:,i+rStep));
+		title(strcat("Temperatura en r=", num2str(1/2 + i*deltaR)) );
+		xlabel("Tiempo");
+		ylabel("Temperatura");
+	endfor
+					
+	print -dpng "-S1100,720" graph_c.png	
+	
+endfunction
+
+
+
+
+
+
