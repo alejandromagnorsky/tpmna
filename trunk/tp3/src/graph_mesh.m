81e36a4bf558a34fb1855c79a26a587b02cef6ac
merchante.mariano <merchante.mariano@c6979bec-28b4-541b-10ca-921024090cd1>
2011-11-28 14:10:07 +0000

    Ejercicio c) terminado.

--- trunk/tp3/src/graph_mesh.m
+++ trunk/tp3/src/graph_mesh.m
@@ -2,35 +2,11 @@ function graph_mesh(deltaR, deltaT)
 
 	v = centeredDifferences(deltaR, deltaT);
 	
-	r = .6:.9;
-	
-	rTotal = (1/2)/deltaR;
-	tTotal = 10/deltaT;
-		
-	%hold;
-	%for i=1:rTotal
-	%		plot(v(:,1), v(:,i));
-	%endfor
-			
 	surf((1/2):deltaR:1,0:deltaT:10,v);
 	
-	% Para los histogramas
-	%e = zeros(n,1);
-	%for j = 1:quant
-      %          for i = 1:n
-	%		e(i) = data(i,j) - v(t(i), x(1,j), x(2,j),x(3,j));
-      %          endfor
-	%	subplot(4,3,j);
-	%	hist(e);
-	%	title(names(j,:));
-      %  endfor
-	  
 	xlabel("Radio del cilindro");
 	ylabel("Tiempo");
 	zlabel("Temperatura");
-	
-	
-	
 	  
 	title("Evolución de la temperatura de un cilindro");
 		
