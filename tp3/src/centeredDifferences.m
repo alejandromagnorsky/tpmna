dbefa2f2a8aee935a64642f076a995ef830d66dd
alejandromagnorsky <alejandromagnorsky@c6979bec-28b4-541b-10ca-921024090cd1>
2011-11-29 20:20:28 +0000

    Esquema

--- tp3/src/centeredDifferences.m
+++ tp3/src/centeredDifferences.m
@@ -15,11 +15,7 @@ function v = centeredDifferences(deltaR, deltaT)
 
 	for m=1:l
 		for k=2:n
-			a = 0;
-			if(m > 1)
-				a = v(m-1,k);	
-			endif
-			v(m+1,k) = 2*v(m,k) - a + 4*K*deltaT^2/deltaR^2 * (v(m,k+1) - 2*v(m,k) + v(m,k-1)) + 4*K*deltaT^2/(2*deltaR*(1/2+k*deltaR)) * (v(m,k+1) - v(m,k-1));
+			v(m+1,k) = v(m,k) + 4*K*deltaT/deltaR^2 * (v(m,k+1) - 2*v(m,k) + v(m,k-1)) + 4*K*deltaT/(2*deltaR*(1/2+k*deltaR)) * (v(m,k+1) - v(m,k-1));
 		endfor
 	endfor
 
